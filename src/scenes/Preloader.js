6e2f230372e4607ad74aec520c963e0312b7c0de
Martin Dib <dibmartin2@gmail.com>
2025-04-26 15:11:33 -0300

    public/assets para probar si anda netlify

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -5,65 +5,65 @@ export class Preloader extends Scene {
   constructor() { super('Preloader'); }
   preload() {
     this.load.setPath('assets');
-    this.load.image('mapita','mapa.png');
-    this.load.image('vidagrande','vidagrande.png');
-    this.load.image('main','princ.png');
-    this.load.image('plataforma','prosa.png');
-    this.load.image('pvio','pvio.png');
-    this.load.image('psal','despramp.png');
-    this.load.image('paz','124.png');
-    this.load.image('pam','3.png');
-    this.load.image('proj','proj.png');
-    this.load.image('pdeath','fondo.png');
-    this.load.image('roca','roca.png');
-    this.load.image('fallRock','bola.png');
-    this.load.image('fire','fire.png');
-    this.load.spritesheet('spring','trampolin.png',{frameWidth:24,frameHeight:14});
-    this.load.image('powerAttack', 'hacha.png');
-    this.load.image('powerFire',   'fuego2.png');
-    this.load.image('bolaAtaque','hacha.png');
-    this.load.image('bolaFuego','fuego4.png');
-    this.load.spritesheet('snail','caracolanim.png',{frameWidth:24,frameHeight:24});
-    this.load.spritesheet('crow','crow.png',{frameWidth:16,frameHeight:15});
-    this.load.image('cobraFireball','cobrafireball.png');
-    this.load.spritesheet('cobra','cobrad.png',{frameWidth:16,frameHeight:23});
-    this.load.spritesheet('kelloGreen','kellogreen.png',{frameWidth:16,frameHeight:16});
-    this.load.spritesheet('kelloBrown','kellobrown.png',{frameWidth:16,frameHeight:16});
-    this.load.image('kelloDeath','deathkello.png');
-    this.load.image('snailDeath','deathcaracol.png');
-    this.load.image('crowDeath','deathcrow.png');
-    this.load.image('cobraDeath','deathcobra.png');
-    this.load.spritesheet('huevoAtaque', 'huevoroto.png', { frameWidth: 22, frameHeight: 16 });
-    this.load.spritesheet('huevoFuego',   'huevoroto.png',   { frameWidth: 22, frameHeight: 16 });
-    this.load.spritesheet('huevoPatineta',   'huevoroto.png',   { frameWidth: 22, frameHeight: 16 });
-    this.load.spritesheet('playerSkate','patineta.png',{frameWidth:24,frameHeight:32});
-    this.load.image('powerpatineta','patin.png');
-    this.load.spritesheet('player','walki.png',{frameWidth:18,frameHeight:32});
-    this.load.spritesheet('idlep','idle.png',{frameWidth:16,frameHeight:32});
-    this.load.spritesheet('jumpp','jumpp.png',{frameWidth:20,frameHeight:32});
-    this.load.spritesheet('playerDeath', 'playerdeath.png', { frameWidth: 24, frameHeight: 24 });
-    this.load.image('apple', 'apple.png');
-    this.load.image('banana', 'banana.png');
-    this.load.image('peach', 'peach.png');
-    this.load.image('carrot', 'carrot.png');
-    this.load.image('score50', '50pts.png');
-    this.load.image('score100','100pts.png');
-    this.load.image('vida','vidachico.png');
-    this.load.image('jarron',  'jarron.png');
-    this.load.image('score1000', '1000pts.png');
-    this.load.spritesheet('axea', 'axespin.png', { frameWidth: 16, frameHeight: 16 });
-    this.load.spritesheet('firea', 'fireflicker.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.image('mapita','public/assets/mapa.png');
+    this.load.image('main','public/assets/princ.png');
+    this.load.image('plataforma','public/assets/prosa.png');
+    this.load.image('pvio','public/assets/pvio.png');
+    this.load.image('psal','public/assets/despramp.png');
+    this.load.image('paz','public/assets/124.png');
+    this.load.image('pam','public/assets/3.png');
+    this.load.image('proj','public/assets/proj.png');
+    this.load.image('pdeath','public/assets/fondo.png');
+    this.load.image('roca','public/assets/roca.png');
+    this.load.image('fallRock','public/assets/bola.png');
+    this.load.image('fire','public/assets/fire.png');
+    this.load.spritesheet('spring','public/assets/trampolin.png',{frameWidth:24,frameHeight:14});
+    this.load.image('powerAttack', 'public/assets/hacha.png');
+    this.load.image('powerFire',   'public/assets/fuego4.png');
+    this.load.image('bolaAtaque','public/assets/hacha.png');
+    this.load.image('bolaFuego','public/assets/fuego4.png');
+    this.load.spritesheet('snail','public/assets/caracolanim.png',{frameWidth:24,frameHeight:24});
+    this.load.spritesheet('crow','public/assets/crow.png',{frameWidth:16,frameHeight:15});
+    this.load.image('cobraFireball','public/assets/cobrafireball.png');
+    this.load.spritesheet('cobra','public/assets/cobrad.png',{frameWidth:16,frameHeight:23});
+    this.load.spritesheet('kelloGreen','public/assets/kellogreen.png',{frameWidth:16,frameHeight:16});
+    this.load.spritesheet('kelloBrown','public/assets/kellobrown.png',{frameWidth:16,frameHeight:16});
+    this.load.image('kelloDeath','public/assets/deathkello.png');
+    this.load.image('snailDeath','public/assets/deathcaracol.png');
+    this.load.image('crowDeath','public/assets/deathcrow.png');
+    this.load.image('cobraDeath','public/assets/deathcobra.png');
+    this.load.spritesheet('huevoAtaque', 'public/assets/huevoroto.png', { frameWidth: 22, frameHeight: 16 });
+    this.load.spritesheet('huevoFuego',   'public/assets/huevoroto.png',   { frameWidth: 22, frameHeight: 16 });
+    this.load.spritesheet('huevoPatineta',   'public/assets/huevoroto.png',   { frameWidth: 22, frameHeight: 16 });
+    this.load.spritesheet('playerSkate','public/assets/patineta.png',{frameWidth:24,frameHeight:32});
+    this.load.image('powerpatineta','public/assets/patin.png');
+    this.load.spritesheet('player','public/assets/walki.png',{frameWidth:18,frameHeight:32});
+    this.load.spritesheet('idlep','public/assets/idle.png',{frameWidth:16,frameHeight:32});
+    this.load.spritesheet('jumpp','public/assets/jumpp.png',{frameWidth:20,frameHeight:32});
+    this.load.spritesheet('playerDeath', 'public/assets/playerdeath.png', { frameWidth: 24, frameHeight: 24 });
+    this.load.image('apple', 'public/assets/apple.png');
+    this.load.image('banana', 'public/assets/banana.png');
+    this.load.image('peach', 'public/assets/peach.png');
+    this.load.image('carrot', 'public/assets/carrot.png');
+    this.load.image('score50', 'public/assets/50pts.png');
+    this.load.image('score100','public/assets/100pts.png');
+    this.load.image('vida','public/assets/vidachico.png');
+    this.load.image('jarron',  'public/assets/jarron.png');
+    this.load.image('score1000', 'public/assets/1000pts.png');
+    this.load.spritesheet('axea', 'public/assets/axespin.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.spritesheet('firea', 'public/assets/fireflicker.png', { frameWidth: 16, frameHeight: 16 });
 
     //solo para juego2
 
-    this.load.image('mapa2','mapasinnada.png');
-    this.load.image('plat21','plat2.png');
-    this.load.image('cuadrao','cuadrado.png');
-    this.load.image('plat22','plat22.png');
-    this.load.image('plat23','plat23.png');
-    this.load.image('plat24','platlvl2.png');
-    this.load.image('plat25','platboss.png');
-    this.load.spritesheet('araña', 'araña.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.image('mapa2','public/assets/mapasinnada.png');
+    this.load.image('plat21','public/assets/plat2.png');
+    this.load.image('cuadrao','public/assets/cuadrado.png');
+    this.load.image('plat22','public/assets/plat22.png');
+    this.load.image('plat23','public/assets/plat23.png');
+    this.load.image('plat24','public/assets/platlvl2.png');
+    this.load.image('plat25','public/assets/platboss.png');
+    this.load.spritesheet('araña', 'public/assets/araña.png', { frameWidth: 16, frameHeight: 16 });
+    this.load.image('arañaDeath','public/assets/deatharaña.png');
   }
   create() {
 
@@ -132,6 +132,11 @@ export class Preloader extends Scene {
       key: 'axeSpin',
       frames: this.anims.generateFrameNumbers('axea',{ start:0, end:3 }),
       frameRate: 10, repeat: -1
+    });
+    this.anims.create({
+      key: 'fireFlicker',
+      frames: this.anims.generateFrameNumbers('firea',{ start:0, end:3 }),
+      frameRate: 12, repeat: -1
     });
     this.anims.create({
       key: 'arañaIdle',
